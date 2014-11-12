c63f5505c6b81c2fdd3562fb4ec7265907f3b61d
dragonqwer <dragonqwer@163.com>
2014-11-12 17:01:40 +0800

    teest2

--- src/com/dragon/planking/widget/HomeFragment.java
+++ src/com/dragon/planking/widget/HomeFragment.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ProgressBar;
-
+//test2
 
 public class HomeFragment extends Fragment
 {
