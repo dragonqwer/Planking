78dcdd1b8cd4f3dd6176131589aef7c96d729468
dragonqwer <dragonqwer@163.com>
2014-12-15 16:35:01 +0800

    去掉没有用的代码

--- src/com/dragon/planking/widget/HomeFragment.java
+++ src/com/dragon/planking/widget/HomeFragment.java
@@ -9,8 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
-import android.widget.ProgressBar;
-//test2
+
 
 public class HomeFragment extends Fragment
 {
